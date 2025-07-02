02d189404d4d2201aa5ac9a1bdfbfb67af119508
DiveshThapa986 <122870955+DiveshThapa986@users.noreply.github.com>
2025-07-02 11:09:07 +0530

    Add files via upload

---
+++ bridgelabz/sum-of-n-elements/Main.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
